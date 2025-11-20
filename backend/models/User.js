16b6f15cb34da1e4f59177cd1dd09b44d760faea
thuhang24 <163641869+23070601@users.noreply.github.com>
2025-11-20 16:31:19 +0700

    Change user_id type to UUID with default value

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const User = sequelize.define('User', {
-  user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  user_id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4 },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
