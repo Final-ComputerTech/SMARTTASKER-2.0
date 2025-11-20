8ede2be9777e6a488994222dcb705eeefd700052
thuhang24 <163641869+23070601@users.noreply.github.com>
2025-11-20 16:37:53 +0700

    Fix import case for userController

--- backend/routers/UserRouters.js
+++ backend/routers/UserRouters.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/UserController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 router.post('/', verifyToken(['Admin']), userController.create);
