e3cbd224ddc1a558542b7d428b57748ca7efc3d6
sfazag <gianghuwowng@gmail.com>
2025-11-25 01:56:14 +0700

    Update Reminder.js

---
+++ backend/models/Reminder.js
@@ -0,0 +1,15 @@
+// models/Reminder.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Reminder = sequelize.define(
+  'Reminder',
+  {
+    reminder_id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4 },
+    task_id: { type: DataTypes.UUID, allowNull: false },
+    reminder_at: { type: DataTypes.DATE, allowNull: false }
+  },
+  { timestamps: true, tableName: 'reminders', underscored: true }
+);
+
+module.exports = Reminder;
