b70dc6a81ccea8dc1dce689278e73d440e8d71be
sfazag <gianghuwowng@gmail.com>
2025-11-25 01:58:50 +0700

    Update Collaborator.js

--- backend/models/Collaborator.js
+++ backend/models/Collaborator.js
@@ -1,4 +1,3 @@
-// models/Collaborator.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
@@ -13,3 +12,4 @@ const Collaborator = sequelize.define(
 );
 
 module.exports = Collaborator;
+
