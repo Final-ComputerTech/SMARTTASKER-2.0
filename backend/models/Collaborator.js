aa424fd00062bcdda15c2318758b5f9170ab2545
sfazag <gianghuwowng@gmail.com>
2025-11-25 01:54:12 +0700

    Update Collaborator.js

---
+++ backend/models/Collaborator.js
@@ -0,0 +1,15 @@
+// models/Collaborator.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Collaborator = sequelize.define(
+  'Collaborator',
+  {
+    collaborator_id: { type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4 },
+    project_id: { type: DataTypes.UUID, allowNull: false },
+    user_id: { type: DataTypes.UUID, allowNull: false }
+  },
+  { timestamps: true, tableName: 'collaborators', underscored: true }
+);
+
+module.exports = Collaborator;
