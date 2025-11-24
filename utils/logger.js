c277140f7e36287f5b2b7b0fc3b7e10f8d621608
sfazag <gianghuwowng@gmail.com>
2025-11-25 02:46:12 +0700

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,12 @@
+function info(msg, meta) {
+  console.log('[INFO]', msg, meta || '');
+}
+function warn(msg, meta) {
+  console.warn('[WARN]', msg, meta || '');
+}
+function error(msg, meta) {
+  console.error('[ERROR]', msg, meta || '');
+}
+
+module.exports = { info, warn, error };
 
